87f3861b634adcb72e8bda561258613074412003
Roymand <chuyirlee@live.com>
2023-11-07 17:11:41 -0600

    Contact page has a form field and contact info, resume has downloadable resume and skillset

--- React-Portfolio/src/components/pages/Contact.jsx
+++ React-Portfolio/src/components/pages/Contact.jsx
@@ -1,7 +1,22 @@
+import './contact.css'
+
 function Contact() {
     return(
         <main>
-        This is my contacts
+            <form id="contact">
+                <h2>Contact Me</h2>
+                <input type="text" placeholder='email'></input>
+                <input type="text" placeholder='name'></input>
+                <textarea placeholder='The contact me field is currently disabled'></textarea>
+                <button>Submit</button>
+            </form>
+            <div id="contactInfo">
+                <h2>Contact Info</h2>
+                <ul>
+                    <li>Email: chuyirlee@gmail.com</li>
+                    <li>Phone Number: -</li>
+                </ul>
+            </div>
         </main>
     )
 }
--- React-Portfolio/src/components/pages/Resume.jsx
+++ React-Portfolio/src/components/pages/Resume.jsx
@@ -1,7 +1,22 @@
+import './resume.css'
+
 function Resume() {
     return(
         <main>
-        This is Resume
+            <div id="resume">
+                <p>
+                    I have worked with
+                </p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>React</li>
+                </ul>
+            </div>
+            <a href="./MyResume.pdf" download="Chuyi Lee Resume 2022" target="_blank" rel="noreferrer">
+                <button id="resumeDL">Download my resume</button>
+            </a>
         </main>
     )
 }
